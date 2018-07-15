8e7840854ad22a1078ae2b9512ea24ceffb74829
Recky-krec <ubuntukrec@gmail.com>
2018-07-15 23:42:19 +0100

    updated for input url and file name

--- Source/DM/main.cpp
+++ Source/DM/main.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <curl/curl.h>
 
-static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+// for testing: https://img.memecdn.com/quack-quack-motherfucker_o_1174873.jpg
+
+static size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream)
 {
     size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
     return written;
@@ -10,31 +12,36 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 
 int main()
 {
-    FILE* pagefile;
+    std::string url, outputFileName;
+    std::cout << "URL: ";
+    std::cin >> url;
+    std::cout << "Output Filename (don't put extension): ";
+    std::cin >> outputFileName;
 
+
+    FILE* pageFile;
 	curl_global_init(CURL_GLOBAL_ALL);
 	auto data = curl_version_info(CURLVERSION_NOW);
 	std::cout << "libcurl " << data->version << " at " << data->host << "\n";
+	auto curlHandle = curl_easy_init();
 
-	auto curl_handle = curl_easy_init();
 
-    curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+	curl_easy_setopt(curlHandle, CURLOPT_VERBOSE, 1L);
     //curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+	curl_easy_setopt(curlHandle, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, write_data);
 
-	curl_easy_setopt(curl_handle, CURLOPT_URL, "https://sv5.onlinevideoconverter.com/download?file=f5h7a0f5a0e4g6j9");
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
 
-
-    pagefile = fopen("file.mp3", "wb");
-    if(pagefile)
+    pageFile = fopen( (outputFileName + std::string(".out")).c_str(), "wb");
+    if(pageFile)
     {
-        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
-        curl_easy_perform(curl_handle);
-        fclose(pagefile);
+        curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, pageFile);
+        curl_easy_perform(curlHandle);
+        fclose(pageFile);
     }
 
 
-	curl_easy_cleanup(curl_handle);
+	curl_easy_cleanup(curlHandle);
   	curl_global_cleanup();
 	return 0;
 }
